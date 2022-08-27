046e1c7b55ba5a1f46e51d42f08f0c9394675109
Max Tao <maxtao989@gmail.com>
2022-08-27 16:00:17 -0700

    upload unit 3

---
+++ src/Unit3/Labs/GradeCheck/Grade.java
@@ -0,0 +1,45 @@
+package Unit3.Labs.GradeCheck;
+
+
+public class Grade
+{
+
+    private double numGrade;
+
+    public Grade()
+    {
+    }
+
+    public Grade(double grade)
+    {
+        this.numGrade = grade;
+    }
+
+    public void setGrade(double grade)
+    {
+
+    }
+
+    public String getLetterGrade( )
+    {
+        String letGrade="";
+        if (numGrade >= 89.5){
+            letGrade = "A";
+        } else if (numGrade >= 79.5){
+            letGrade = "B";
+        } else if (numGrade >= 69.5){
+            letGrade = "C";
+        } else if (numGrade >= 59.5){
+            letGrade = "D";
+        } else {
+            letGrade = "F";
+        }
+
+        return letGrade;
+    }
+
+    public String toString()
+    {
+        return String.format("%.2f",numGrade) + " is a " + getLetterGrade();
+    }
+}
---
+++ src/Unit3/Labs/GradeCheck/Main.java
@@ -0,0 +1,35 @@
+package Unit3.Labs.GradeCheck;
+
+import java.util.Scanner;
+class Main
+{
+
+    public static void main(String[] args)
+    {
+        // for each test case...
+
+        // create a new Grade object
+        // call the Grade object to get the letter grade
+        // print out the result
+        //  - note you can just print the object which will call toString
+        Scanner s = new Scanner(System.in);
+        double t1 = 78;
+        double t2 = 92.5;
+        double t3 = 31;
+        double t4 = 89.5;
+        double t5 = 89.49;
+
+        Grade g1 = new Grade(t1);
+        Grade g2 = new Grade(t2);
+        Grade g3 = new Grade(t3);
+        Grade g4 = new Grade(t4);
+        Grade g5 = new Grade(t5);
+
+
+        System.out.println(g1.toString());
+        System.out.println(g2.toString());
+        System.out.println(g3.toString());
+        System.out.println(g4.toString());
+        System.out.println(g5.toString());
+    }
+}
