fb2f308168699829984a656c420fef3abc90b7cf
Max Tao <maxtao989@gmail.com>
2022-09-01 18:20:29 -0700

    Unit 4 lab code

--- src/Unit4/Labs/DecreasingWord/TriangleOne.java
+++ src/Unit4/Labs/DecreasingWord/TriangleOne.java
@@ -1,2 +1,28 @@
-package Unit4.Labs.DecreasingWord;public class TriangleOne {
+package Unit4.Labs.DecreasingWord;
+
+public class TriangleOne
+{
+    private String word;
+
+    public TriangleOne()
+    {
+        word="";
+    }
+
+    public TriangleOne(String s)
+    {
+        word = s;
+    }
+
+    public void setWord(String s)
+    {
+    }
+
+    public void print( )
+    {
+        for (int i = word.length(); i >= 0; i--){
+            System.out.println(word.substring(0, i));
+        }
+    }
+
 }
